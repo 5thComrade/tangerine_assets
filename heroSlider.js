9b96a31b3ba567f1575ba116158102f59558fd88
5thComrade <antonychiramel07@gmail.com>
2021-07-02 21:02:53 +0530

    hero slider images added

---
+++ heroSlider.js
@@ -0,0 +1,31 @@
+const heroSlider = [
+    {
+        id: 001,
+        name: "Sleeping Saint",
+        artist: "Arpana Caur",
+        dimension: "30\" x 58\"",
+        description: "Oil on Canvas",
+        year: 1992,
+        folder: "hero-slider-images"
+    },
+    {
+        id: 002,
+        name: "Garden in Heaven",
+        artist: "Nayana Kanodia",
+        dimension: "30\" x 58\"",
+        description: "Oil on Canvas",
+        year: 2016,
+        folder: "hero-slider-images"
+    },
+    {
+        id: 003,
+        name: "Prison Break",
+        artist: "Sunil Padwal",
+        dimension: "30\" x 58\"",
+        description: "Oil on Canvas",
+        year: 2001,
+        folder: "hero-slider-images"
+    },
+]
+
+export default heroSlider
